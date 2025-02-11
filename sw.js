2d34ecd9e7c9fb1ce57c495311c0fcb071d60dc1
Herdi88 <127148030+Herdi88@users.noreply.github.com>
2025-02-12 02:02:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,11 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('hospital-cache').then(cache => {
-            return cache.addAll([
-                '/index.html',
-                '/style.css',
-                '/app.js',
-                '/manifest.json'
-            ]);
-        })
-    );
+self.addEventListener("install", event => {
+    console.log("Service Worker installing...");
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("activate", event => {
+    console.log("Service Worker activated...");
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(fetch(event.request));
 });
